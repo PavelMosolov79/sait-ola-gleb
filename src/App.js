e108b359a67b9dee1dc0d518938bf5e3c1e3d3b5
Ksardas <78637178+PavelMosolov79@users.noreply.github.com>
2025-08-11 10:05:28 +0700

    New

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Paper from "./assets/paper.png";
+import PaperTwo from "./assets/paperTwo.png";
+import Gleb from "./assets/gleb.png";
+import Ola from "./assets/ola.png"
+
+
+function App() {
+  return (
+    <div className="App">
+      <div 
+        style={{ backgroundImage: `url(${Paper})` }}
+        className='one-block'
+      >
+        <h1>wedding</h1>
+        <h2>06.09.2025</h2>
+        <h3>ГЛЕБ & ОЛЬГА</h3>
+        <div className='persons'>
+          <img className='personOne' src={Ola}/>
+          <img className='personTwo' src={Gleb}/>   
+        </div>
+      </div>
+      <div
+        style={{ backgroundImage: `url(${PaperTwo})` }}
+        className='one-block'
+      >
+        <div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
